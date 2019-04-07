d6210da6fec36c4855bd2ba0d8533ba9d44ba0da
Thaya <hayafluss@gmail.com>
2019-04-08 01:49:25 +0900

    gitignroe

---
+++ Untitled-2.py
@@ -0,0 +1,26 @@
+
+#%%
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+unsorted = [3,6,8,10,1,2,1]
+sorted = quicksort()
+print(sorted)
+# Prints "[1, 1, 2, 3, 6, 8, 10]"
+
+
+
+
+#%%
+import os
+
+#%%
+os.path
+
+#%%
